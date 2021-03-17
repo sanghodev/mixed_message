6f1f1ea901a163434fa0947a0598f9aa1e25401f
sanghodev <sanghodev@gmail.com>
2021-03-17 12:00:26 -0400

    Start

---
+++ mixed_message.js
@@ -0,0 +1,30 @@
+// My First Project Start
+
+/*
+1. Make three different pieces of data for random answer.
+
+Your sign is sun.
+
+You are having good luck. 
+
+You should: "trust no one"
+*/
+
+//let messageBox = ['Your sign is sun.', 'You are having good luck.', 'You should: "trust no one"'];
+
+//messageBox.random(console.log('Text:'))
+/*
+const messagePick = (messageBox) => {
+   // for (let i=0;i<message.length;i++) {}
+   
+}
+*/
+//messageBox();
+console.log('er');
+
+let x = 1
+let y = 2
+
+let num = x + y
+
+console.log(1 + 2);
